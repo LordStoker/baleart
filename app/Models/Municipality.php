4787bce0c1e46db38eeb4d882eee17554fb4749a
Lord Stoker <tpc00@iesemilidarder.com>
2024-11-15 19:32:31 +0100

    classes import

--- app/Models/Municipality.php
+++ app/Models/Municipality.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\Address;
 use Illuminate\Database\Eloquent\Model;
 
 class Municipality extends Model
 {
-    //
+    public function addresses(){
+        return $this->hasMany(Address::class);
+    }
 }
