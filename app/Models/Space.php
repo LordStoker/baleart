669c51b17b28c25061f944cc2cb3563ee01cb0ba
Lord Stoker <tpc00@iesemilidarder.com>
2024-11-15 19:39:22 +0100

    space class relations defined

--- app/Models/Space.php
+++ app/Models/Space.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
+use App\Models\Address;
+use App\Models\Service;
+use App\Models\Modality;
+use App\Models\SpaceType;
+use App\Models\SpaceUser;
 use Illuminate\Database\Eloquent\Model;
 
 class Space extends Model
 {
-    //
+    protected function space_type()
+    {
+        return $this->belongsTo(SpaceType::class);
+    }
+
+    protected function modalities()
+    {
+        return $this->belongsToMany(Modality::class);
+    }
+
+    protected function services()
+    {
+        return $this->belongsToMany(Service::class);
+    }
+
+    protected function address()
+    {
+        return $this->belongsTo(Address::class);
+    }
+    
+    protected function space_users()
+    {
+        return $this->hasMany(SpaceUser::class);
+    }
 }
