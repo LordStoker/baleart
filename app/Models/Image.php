5eaec4315df1c63417af843e8d4fc497c811f1ba
Lord Stoker <tpc00@iesemilidarder.com>
2024-11-15 19:25:05 +0100

    Image class relation defined

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\SpaceUser;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    protected function space_user(){
+        return $this->belongsTo(SpaceUser::class);
+    }
 }
