43665194a4ba5979a149cee6f4fe5f6b93e55cfb
Lord Stoker <tpc00@iesemilidarder.com>
2024-11-15 19:34:44 +0100

    rol class relation defined

--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Rol extends Model
 {
-    //
+    protected function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
