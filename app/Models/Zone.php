76d5064d41e90322c851cef2e197f6d626644fdf
Lord Stoker <tpc00@iesemilidarder.com>
2024-11-15 19:49:08 +0100

    zone class relation defined

--- app/Models/Zone.php
+++ app/Models/Zone.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Zone extends Model
 {
-    //
+    protected function addresses(){
+        
+        return $this->hasMany(Address::class);
+    }
 }
