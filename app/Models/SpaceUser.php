6e216a178d6b2a0bffa9de140113890d9c34fa11
Lord Stoker <tpc00@iesemilidarder.com>
2024-11-15 19:44:36 +0100

    spaceuser class relations defined

--- app/Models/SpaceUser.php
+++ app/Models/SpaceUser.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\Image;
+use App\Models\Space;
 use Illuminate\Database\Eloquent\Model;
 
 class SpaceUser extends Model
 {
-    //
+    public function images(){
+        return $this->hasMany(Image::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function space(){
+        return $this->belongsTo(Space::class);
+    }
 }
