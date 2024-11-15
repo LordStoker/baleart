8fca1a31b8510de495b6c5ea7d8e97c4e5e6b20b
Lord Stoker <tpc00@iesemilidarder.com>
2024-11-15 19:23:04 +0100

    Address class defined

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use App\Models\Zone;
+use App\Models\Space;
+use App\Models\Municipality;
 use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    protected function municipality()
+    {
+        return $this->belongsTo(Municipality::class);
+    }
+
+    protected function zone()    
+    {
+        return $this->belongsTo(Zone::class);
+    }
+
+    protected function spaces()
+    {
+        return $this->hasMany(Space::class);
+    }
 }
