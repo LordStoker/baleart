b3a3adc503d795ca342a50c64aefd24dd28f9248
Lord Stoker <tpc00@iesemilidarder.com>
2024-11-15 19:35:23 +0100

    service class relation defined

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Space;
 use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    protected function spaces()
+    {
+        return $this->belongsToMany(Space::class);
+    }
 }
