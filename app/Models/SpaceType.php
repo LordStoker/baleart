52d91d522ad28e85f23ae16a53f5237009febdf1
Lord Stoker <tpc00@iesemilidarder.com>
2024-11-15 19:41:24 +0100

    spacetype class relation defined

--- app/Models/SpaceType.php
+++ app/Models/SpaceType.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Space;
 use Illuminate\Database\Eloquent\Model;
 
 class SpaceType extends Model
 {
-    //
+    public function spaces()
+    {
+        return $this->hasMany(Space::class);
+    }
 }
