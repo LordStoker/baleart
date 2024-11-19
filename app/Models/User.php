781913ded87367de6843e1351032bd16686aa2b1
LordStoker <tpc00@iesemilidarder.com>
2024-11-19 20:18:57 +0100

    delete import from user

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ namespace App\Models;
 use App\Models\Rol;
 use App\Models\Space;
 use App\Models\Comment;
-use App\Models\SpaceUser;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
