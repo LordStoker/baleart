a8aec4780d247bdf8afa4fc97027e942c5a97386
LordStoker <tpc00@iesemilidarder.com>
2024-11-21 15:47:52 +0100

    Seeder para users admin y gestores

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,36 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //User Admin
+        $adminUser = new User();
+        $adminUser->name = 'Admin';
+        $adminUser->lastName = 'Istrador';
+        $adminUser->email = 'admin@admin.com';
+        $adminUser->phone = '666666666';
+        $adminUser->password = 'admin123';
+        $adminUser->role_id = 1;
+        $adminUser->save();
+
+        //Users Gestores
+
+        $jsonData = file_get_contents("C:\\temp\\baleart\\usuaris.json");
+        $users = json_decode($jsonData, true);
+        if ($jsonData === false || $users === null) {
+            throw new \Exception("Error al leer o procesar el JSON.");
+        }
+        foreach ($users['usuaris']['usuari'] as $user) {
+            $gestorUser = new User();
+            $gestorUser->name = $user['nom'];
+            $gestorUser->lastName = $user['llinatges'];
+            $gestorUser->email = $user['email'];
+            $gestorUser->phone = $user['telefon'];
+            $gestorUser->password = $user['password'];
+            $gestorUser->role_id = 2;
+            $gestorUser->save();
+        }
+
+
+
+        
     }
 }
