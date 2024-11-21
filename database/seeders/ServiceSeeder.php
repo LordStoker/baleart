1d39eb070b4a831e4d8420a2bdbe7cd3315f09d6
LordStoker <tpc00@iesemilidarder.com>
2024-11-21 17:28:42 +0100

    ServiceSeeder(json)

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Service;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class ServiceSeeder extends Seeder
 {
@@ -12,6 +13,19 @@ class ServiceSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $jsonData = file_get_contents("C:\\temp\\baleart\\serveis.json");
+        $services = json_decode($jsonData, true);
+        if ($jsonData === false || $services === null) {
+            throw new \Exception("Error al leer o procesar el JSON.");
+        }
+        foreach ($services['serveis']['servei'] as $servei) {
+            $service = new Service();
+            $service->name = $servei['cat'];
+            $service->description_CA = $servei['cat'];
+            $service->description_ES = $servei['esp'];
+            $service->description_EN = $servei['eng'];
+            $service->save();
+        }
+
     }
 }
