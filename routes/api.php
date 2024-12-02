b9201bc8c001345f5795a92bc750484ffe3122ae
Lord Stoker <tpc00@iesemilidarder.com>
2024-12-02 22:15:06 +0100

    creación de controladures y sus rutas para la api

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,30 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ZoneController;
+use App\Http\Controllers\Api\ImageController;
+use App\Http\Controllers\Api\SpaceController;
+use App\Http\Controllers\Api\IslandController;
+use App\Http\Controllers\Api\AddressController;
+use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\Api\ServiceController;
+use App\Http\Controllers\Api\ModalityController;
+use App\Http\Controllers\Api\SpaceTypeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiresource('user', UserController::class);
+Route::apiresource('role', RoleController::class);
+Route::apiresource('space', SpaceController::class);
+Route::apiresource('comment', CommentController::class);
+Route::apiresource('address', AddressController::class);
+Route::apiresource('image', ImageController::class);
+Route::apiresource('island', IslandController::class);
+Route::apiresource('modality', ModalityController::class);
+Route::apiresource('service', ServiceController::class);
+Route::apiresource('spacetype', SpaceTypeController::class);
+Route::apiresource('zone', ZoneController::class);
