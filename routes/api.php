b36fdf1981e55fbeca1cdf3d0e648484adac1b76
LordStoker <tpc00@iesemilidarder.com>
2024-12-02 20:03:35 +0100

    controlador de usuario y rutas de todos los métodos en api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiresource('user', UserController::class);
